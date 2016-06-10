abd499f81812883d08f3b86e2538ad404f9d0d28
David Rubin <dudrubin@gmail.com>
2016-06-10 14:17:15 +0300

    Initial commit

---
+++ B16 Ex06 DudiRubin 039532908 AmitaiHandler 039213228/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace B16_Ex06_DudiRubin_039532908_AmitaiHandler_039213228
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
